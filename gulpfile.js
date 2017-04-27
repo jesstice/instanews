4b7984825596a40a911f09943b0530b11532654f
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-04-27 16:41:12 -0700

    udpated storiesList js and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,4 @@
-// TERMINAL ACTIONS:
-// Start with npm init
-// npm install gulp plug-ins required
 
-// All gulp packages must be named in require() as the plugin is named
 var gulp = require('gulp'),
 		uglify = require('gulp-uglify'),
 		rename = require('gulp-rename'),
@@ -13,13 +9,10 @@ var gulp = require('gulp'),
 		cssnano = require('gulp-cssnano'),
 		prettyerror = require('gulp-prettyerror');
 
-// the default task runs the tasks listed in the array
 gulp.task('default', ['watch', 'browser-sync']);
 
-// compiles scss to css
 gulp.task('sass', function() {
 	gulp.src('./sass/style.scss')
-		// .pipe() is a node method (and general computing concept)	
 		.pipe(prettyerror())
 		.pipe(sass())
 		.pipe(autoprefixer({
@@ -30,18 +23,13 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('./build/css'))
 });
 
-// define task to uglify and move js files to build dir
 gulp.task('scripts', ['lint'], function() {
-	// *.js ensures that all the .js files are pulled
 	gulp.src('./js/*.js')
 		.pipe(uglify())
-		// takes js files and renames
 		.pipe(rename({ extname: '.min.js' }))
-		// need to place new files in specific newly created dir
 		.pipe(gulp.dest('./build/js'))
 });
 
-// runs eslint to see if code follows rules
 gulp.task('lint', function() {
 	return gulp.src(['./js/*.js','!node_modules/**'])
     .pipe(eslint())
@@ -49,15 +37,11 @@ gulp.task('lint', function() {
     .pipe(eslint.failAfterError());
 });
 
-// define task to watch for changes to a file
 gulp.task('watch', function() {
-	// 1st argument: where we want to watch
-	// 2nd argument: what to do when things change
 	gulp.watch('./js/*.js', ['scripts']);
 	gulp.watch('./sass/*.scss', ['sass']);
 });
 
-// define task to refresh browser automatically
 gulp.task('browser-sync', function() {
 	browserSync.init({
 		server: {
--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
-$(function() {
+$(function () {
 
-	$('#section-options').change(function(event) {
-    event.preventDefault();
-		
+	$('#section-options').change(function (event) {
+		event.preventDefault();
+
 		var chosenSection = $('#section-options option').filter(':selected').val();
-
 		var url = 'https://api.nytimes.com/svc/topstories/v2/' + chosenSection + '.json';
 		url += '?' + $.param({
 			'api-key': 'e44f8396e12a4c7c86f4a9cc6884330e'
 		});
-		
-	// Header changes
+		$('.generated-stories').empty();
 
-	// Loading gif
+		// Header changes
+
+		// Loading gif
 		$('.generated-stories').before('<img src="./assets/images/ajax-loader.gif" alt="loading" id="loading-gif">');
 
-		var storiesList = '';
+		var storiesList = [];
 
-	// Request stories
+		// Request stories
 		$.ajax({
 			method: 'GET',
 			url: url,
-		}).done(function(data) {
-			$.each(data.results, function(index, value) {
-				for (var i = 0; i < 12; i++ ) {
-					if (value['multimedia']['url'] != '' ) {
-						storiesList += value.abstract;						
-					}
+		}).done(function (data) {
+			$.each(data.results, function (index, value) {
+				if (value.multimedia.length !== 0) {
+					storiesList.push(
+						'<li class="story">' + '<img class="story-image" src="' + value.multimedia[4].url + '"/img>' + '<p>' + value.abstract + '</p>' + '</li>'
+					);
 				}
-				// '<img src="' + value.artworkUrl100 + '"/img>'
 			});
-			console.log(storiesList);
-			// $('.generated-stories').append(storiesList);
-		}).fail(function() {
+			var firstTwelve = storiesList.slice(0, 12).join('');
+			$('.generated-stories').append(firstTwelve);
+		}).fail(function () {
 			$('.generated-stories').append('Sorry. Looks like something isn\'t working right!');
-		}).always(function() {
+		}).always(function () {
 			$('#loading-gif').remove();
 		});
 
